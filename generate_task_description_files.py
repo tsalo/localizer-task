e47cb3e3890c993b8118c5c014912d8c1d585951
Taylor Salo <tsalo006@fiu.edu>
2019-09-17 12:45:08 -0400

    Add jsons.

---
+++ generate_task_description_files.py
@@ -0,0 +1,23 @@
+import json
+
+descriptions = {
+    'trial_number': {
+        'LongName': 'trial number',
+        'Description': 'Index of the trial in time.'
+    },
+    'tap_count': {
+        'LongName': 'Tap count',
+        'Description': 'Number of button presses within duration of trial, including fixation following presentation of the stimulus.'
+    },
+    'tap_duration': {
+        'LongName': 'Tap duration',
+        'Description': 'Duration of finger-tapping, from first button press to last button press within duration of trial and following fixation.',
+        'Units': '[s] second'
+    }
+}
+
+with open('task-primaryEstimation_events.json', 'w') as fo:
+    json.dump(descriptions, fo, sort_keys=True, indent=4)
+
+with open('task-primaryDetection_events.json', 'w') as fo:
+    json.dump(descriptions, fo, sort_keys=True, indent=4)
