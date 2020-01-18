c9eed297b6ddc755a2f66094af8ffe2201cd4d7f
Taylor Salo <tsalo006@fiu.edu>
2020-01-18 14:10:36 -0500

    Improve timing.

--- generate_config_files.py
+++ generate_config_files.py
@@ -12,6 +12,8 @@ Originally created by Jakub Kaczmarzyk and adapted to combine tasks.
 from __future__ import division, print_function
 import numpy as np
 import pandas as pd
+from scipy.stats import gumbel_r
+import random
 
 # These tracks are 20 seconds long.
 # 10s versions created by
@@ -59,14 +61,36 @@ END_DUR = 6  # fixation after trials
 # total time = TASK_TIME + START_DUR + END_DUR = 450 = 7.5 mins
 
 
+def randomize_carefully(elems, n_repeat=2):
+    """
+    Shuffle without consecutive duplicates
+    From https://stackoverflow.com/a/22963275/2589328
+    """
+    s = set(elems)
+    res = []
+    for n in range(n_repeat):
+        if res:
+            # Avoid the last placed element
+            lst = list(s.difference({res[-1]}))
+            # Shuffle
+            random.shuffle(lst)
+            lst.append(res[-1])
+            # Shuffle once more to avoid obvious repeating patterns in the last position
+            lst[1:] = random.sample(lst[1:], len(lst)-1)
+        else:
+            lst = elems[:]
+            random.shuffle(lst)
+        res.extend(lst)
+    return res
+
+
 def detection_timing():
-    block_dur = 14.5
-    rest_dur = 14.5
+    block_dur = 14
+    rest_dur = 14
     durs = [block_dur] * N_BLOCKS * N_CONDS
     itis = [rest_dur] * N_BLOCKS * N_CONDS
-    trial_types = list(TRIAL_DICT.keys()) * N_BLOCKS
+    trial_types = randomize_carefully(list(TRIAL_DICT.keys()), N_BLOCKS)
     trial_types = [TRIAL_DICT[tt] for tt in trial_types]
-    np.random.shuffle(trial_types)
     timing_info = np.vstack((durs, itis, trial_types)).T
     timing_df = pd.DataFrame(columns=['duration', 'iti', 'trial_type'],
                              data=timing_info)
@@ -79,40 +103,37 @@ def estimation_timing(seed=None):
     durations and intertrial intervals based on a uniform distribution.
     The process is iterative to minimize the amount of duration lost
     """
-    length = (np.average(DUR_RANGE) + np.average(ITI_RANGE)) * N_TRIALS
-    if np.abs((length * N_CONDS) - TASK_TIME) > 3:
-        raise Exception('Inputs do not seem compatible with total desired '
-                        'time. Proposed length: {}'.format(length * N_CONDS))
-    missing_time_per_cond = np.finfo(dtype='float64').max
+    mu = 4.
+    raw_itis = gumbel_r.rvs(size=100000, loc=mu, scale=1)
+    possible_itis = np.round(raw_itis, 1)
+    # crop to 2-8s
+    possible_itis = possible_itis[possible_itis >= 2]
+    possible_itis = possible_itis[possible_itis <= 8]
+
+    N_TRIALS_TOTAL = N_TRIALS * N_CONDS
+
+    missing_time = np.finfo(dtype='float64').max
     if not seed:
         seed = np.random.randint(1000, 9999)
 
-    while not np.isclose(missing_time_per_cond, 0.0, atol=.5):
+    while (not np.isclose(missing_time, 0.0, atol=10)) or (missing_time < 0):
         state = np.random.RandomState()
-        trial_durs = state.uniform(DUR_RANGE[0], DUR_RANGE[1], N_TRIALS)
-        trial_durs = np.round(trial_durs, 1)
-        trial_itis = state.uniform(ITI_RANGE[0], ITI_RANGE[1], N_TRIALS)
-        trial_itis = np.round(trial_itis, 1)
-        missing_time_per_cond = length - np.sum(trial_durs + trial_itis)
+        durations = state.uniform(DUR_RANGE[0], DUR_RANGE[1], N_TRIALS_TOTAL)
+        durations = np.round(durations, 1)
+
+        itis = state.choice(possible_itis, size=N_TRIALS_TOTAL, replace=True)
+        missing_time = TASK_TIME - np.sum(durations + itis)
         seed += 1
 
     # Fill in one trial's ITI with missing time for constant total time
-    missing_time_per_cond += (TASK_TIME / N_CONDS) - length
-    trial_itis[-1] += missing_time_per_cond
+    missing_time = TASK_TIME - np.sum(durations + itis)
+    itis[-1] += missing_time
 
-    all_cond_trial_durs = [np.random.permutation(trial_durs) for _ in range(N_CONDS)]
-    all_cond_trial_itis = [np.random.permutation(trial_itis) for _ in range(N_CONDS)]
-    trials = list(range(1, N_CONDS + 1)) * N_TRIALS
-    np.random.shuffle(trials)
-    durations = []
-    itis = []
-    c = {t: 0 for t in np.unique(trials)}
-    for condition in trials:
-        durations.append(all_cond_trial_durs[condition-1][c[condition]])
-        itis.append(all_cond_trial_itis[condition-1][c[condition]])
-        c[condition] += 1
+    # trial_types = list(range(1, N_CONDS + 1)) * N_TRIALS
+    # np.random.shuffle(trial_types)
+    trial_types = randomize_carefully(list(range(1, N_CONDS + 1)), N_TRIALS)
 
-    trial_types = [TRIAL_DICT[t] for t in trials]
+    trial_types = [TRIAL_DICT[t] for t in trial_types]
     timing_dict = {
         'duration': durations,
         'iti': itis,
@@ -162,8 +183,9 @@ def main():
             print('    Compiling session {0}'.format(ses))
             for ttype in ttypes:
                 print('\tCompiling {0} task'.format(ttype))
-                print('\t   Updating seed to {0}'.format(seed))
                 df, seed = determine_timing(ttype, seed=seed)
+                if ttype == 'Estimation':
+                    print('\t   Updating seed to {0}'.format(seed))
 
                 df.to_csv('config/sub-{0}_ses-{1}_task-localizer{2}_run-01_'
                           'config.tsv'.format(sub.zfill(2), ses.zfill(2), ttype),
